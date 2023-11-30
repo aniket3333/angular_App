18bbf817a0561f00c39e057b1df8ca85601e9f5d
@anikethiware3 <anikethiware3@gmail.com>
2023-12-01 02:40:54 +0530

    Lazy Loading

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+ title :string = " My Angular App";
+ selectedOption: string = 'def'; // default value for dropdown
+
+ onSelectChange(): void {
+   console.log('Selected option:', this.selectedOption);
+  
+ }
+
+ 
+constructor(){}
+
+ngOnInit(){
+
+}
+
 
 }
